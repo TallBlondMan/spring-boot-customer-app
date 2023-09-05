e6f0e0db2dd83a72dfb2d048655f9b0dc290905d
Mateusz <smetmateusz@gmail.com>
2023-09-05 15:32:21 +0200

    Even more CORS

--- customer-frontend/src/http-common.js
+++ customer-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://app-backend:8080/api",
+    baseURL: "http://10.6.0.232:8080/api",
     headers: {
         'Content-Type': 'application/json'
     }
