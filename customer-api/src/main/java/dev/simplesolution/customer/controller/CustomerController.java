c05310bcb2166887a19a0fc072b86fcb046904a8
Mateusz <smetmateusz@gmail.com>
2023-09-08 14:35:54 +0200

    Added MVC and testing

--- customer-api/src/main/java/dev/simplesolution/customer/controller/CustomerController.java
+++ customer-api/src/main/java/dev/simplesolution/customer/controller/CustomerController.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.simplesolution.customer.entity.Customer;
 import dev.simplesolution.customer.repository.CustomerRepository;
 
-@RestController
-@CrossOrigin // (origins = "${crossorigin.origin}")
+@RestController // (origins = env.getProperty("cors.url"))
 @RequestMapping("/api")
 public class CustomerController {
 	
