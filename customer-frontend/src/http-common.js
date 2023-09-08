aee22e00f821bc84cdcfc14bf17c1dba797314cb
Mateusz <smetmateusz@gmail.com>
2023-09-08 12:51:42 +0200

    Added server IP as environment variable

--- customer-frontend/src/http-common.js
+++ customer-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.API_BASE_URL || "http://10.6.0.232:8080/api",
+    baseURL: process.env.API_BASE_URL || "http://localhost:8080/api",
     headers: {
         'Content-Type': 'application/json'
     }
