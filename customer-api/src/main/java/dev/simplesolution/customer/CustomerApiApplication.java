f6c366c2f55ce6c2343248bbdbd03471095ae893
Mateusz <smetmateusz@gmail.com>
2023-09-12 10:21:55 +0200

    Back to normal - fixed a bug

--- customer-api/src/main/java/dev/simplesolution/customer/CustomerApiApplication.java
+++ customer-api/src/main/java/dev/simplesolution/customer/CustomerApiApplication.java
@@ -1,14 +1,10 @@
 package dev.simplesolution.customer;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerApiApplication {
-
-	@Value("${cors.allowed-origins}")
-	private String allowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApiApplication.class, args);
