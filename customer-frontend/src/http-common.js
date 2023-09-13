c00c729ef06f161278d09c3a4f8e9895dde9d542
Mateusz <smetmateusz@gmail.com>
2023-09-13 14:00:28 +0200

    removed /api/ added static IP in VUE to test Backend

--- customer-frontend/src/http-common.js
+++ customer-frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
+    baseURL: process.env.VUE_APP_BASE_URL || "http://10.6.0.232:8080/api",
     headers: {
         'Content-Type': 'application/json'
     }
--- customer-frontend/src/services/CustomerDataService.js
+++ customer-frontend/src/services/CustomerDataService.js
@@ -2,23 +2,23 @@ import http from '../http-common'
 
 class CustomerDataService {
     getAll() {
-        return http.get('/api/customers')
+        return http.get('/customers')
     }
 
     get(id) {
-        return http.get(`/api/customers/${id}`)
+        return http.get(`/customers/${id}`)
     }
 
     create(data) {
-        return http.post('/api/customers', data)
+        return http.post('/customers', data)
     }
 
     update(id, data) {
-        return http.put(`/api/customers/${id}`, data)
+        return http.put(`/customers/${id}`, data)
     }
 
     delete(id) {
-        return http.delete(`/api/customers/${id}`)
+        return http.delete(`/customers/${id}`)
     }
 }
 
