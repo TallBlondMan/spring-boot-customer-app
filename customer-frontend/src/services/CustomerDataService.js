73aceeba5cc7d37a8b9ee685b1bb94564676e50a
Mateusz <smetmateusz@gmail.com>
2023-09-13 13:36:27 +0200

    Added /api/ to CusDatServ.js

--- customer-frontend/src/services/CustomerDataService.js
+++ customer-frontend/src/services/CustomerDataService.js
@@ -2,23 +2,23 @@ import http from '../http-common'
 
 class CustomerDataService {
     getAll() {
-        return http.get('/customers')
+        return http.get('/api/customers')
     }
 
     get(id) {
-        return http.get(`/customers/${id}`)
+        return http.get(`/api/customers/${id}`)
     }
 
     create(data) {
-        return http.post('/customers', data)
+        return http.post('/api/customers', data)
     }
 
     update(id, data) {
-        return http.put(`/customers/${id}`, data)
+        return http.put(`/api/customers/${id}`, data)
     }
 
     delete(id) {
-        return http.delete(`/customers/${id}`)
+        return http.delete(`/api/customers/${id}`)
     }
 }
 
